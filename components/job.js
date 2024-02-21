2c30502d4b373122a8cdc340e40d5de9c6c4129c
jelly <moodnightsummer@naver.com>
2024-02-21 20:11:18 +0900

    fix : career 페이지 1차 완료

--- components/job.js
+++ components/job.js
@@ -1,11 +1,14 @@
 import Career from "../contents/career";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
 
 const classes = {
   wrap: "mt-4",
   titleText: "text-xl tracking-tighter font-normal",
   periodText: "text-lg text-gray-400 font-light mb-4",
-  smallText: "text-sm text-gray-800 font-light mb-4",
-  normalText: "text-normal text-gray-800",
+  smallText: "text-sm text-gray-800",
+  miniTitleText: "text-normal text-gray-800 font-bold mr-4",
+  jobText: "text-normal text-gray-600 mb-1",
 };
 
 export default function Job() {
@@ -14,25 +17,33 @@ export default function Job() {
       {Career.map((item, idx) => (
         <div key={idx}>
           {item.history.map((result, index) => (
-            <div key={index} className={classes.wrap}>
-              <p key={index} className={classes.titleText}>
-                {result.title}
-              </p>
-              <p key={index} className={classes.periodText}>
-                {result.workPeriod}
-              </p>
-              <p key={index} className={`${classes.normalText} flex-auto`}>
-                팀원 : {result.people}
-              </p>
-              <p key={index} className={`${classes.normalText} flex-auto`}>
-                구성원 : {result.member}
-              </p>
-              <p key={index} className={classes.smallText}>
-                사용 기술 : {result.skills.join(", ")}
-              </p>
-              <p key={index} className={classes.normalText}>
-                {result.jobDescription}
-              </p>
+            <div key={index} className="mb-10">
+              <p className={classes.titleText}>{result.title}</p>
+              <p className={classes.periodText}>{result.workPeriod}</p>
+              <div className="mb-4">
+                <div className="flex items-center">
+                  <p className={classes.miniTitleText}>팀원</p>
+                  <p className={classes.smallText}>{result.people}</p>
+                </div>
+                <div className="flex items-center">
+                  <p className={classes.miniTitleText}>구성</p>
+                  <p className={classes.smallText}>{result.member}</p>
+                </div>
+                <div className="flex items-center">
+                  <p className={classes.miniTitleText}>기술</p>
+                  <p className={classes.smallText}>
+                    {result.skills.join(", ")}
+                  </p>
+                </div>
+              </div>
+              {result.jobDescription.map((job, index) => (
+                <p key={index} className={classes.jobText}>
+                  <span className="mr-3">
+                    <FontAwesomeIcon icon={faPlayCircle} />
+                  </span>
+                  {job}
+                </p>
+              ))}
             </div>
           ))}
         </div>
