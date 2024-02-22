618d7ab74aecfca29df36f0341bc374d32557741
jelly <moodnightsummer@naver.com>
2024-02-22 16:55:29 +0900

    fix : sidebar sticky 처리

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import Profile from "./profile";
 
 export default function Layout(children) {
   return (
-    <div className="h-screen p-4 flex-col flex justify-between items-center">
+    <div className="h-screen p-4 flex-col flex justify-between items-center sticky top-0">
       <title>신재일 포트폴리오</title>
       <Profile />
       <ContentLayout title="Contact" isUnderbar={false}>
--- pages/ais.js
+++ pages/ais.js
@@ -2,12 +2,17 @@ import CareerLayout from "@/components/careerLayout";
 import Layout from "@/components/layout";
 export default function Ais() {
   return (
-    <div className="flex flex-col md:flex-row">
-      <div className="md:col-span-1 p-4 bg-gray-200 h-screen md:flex-shrink-0 z-50">
+    <div className="flex flex-col md:flex-row sticky top-0">
+      {/* Layout 영역 */}
+      <div className="md:col-span-1 p-4 bg-gray-200 md:w-2/8 h-full md:flex-shrink-0 z-50 sticky top-0">
         <Layout />
       </div>
-      <div className="md:col-span-1 m-8 basis-full basis-4/6">
-        <CareerLayout />
+
+      {/* 나머지 두 개의 영역 */}
+      <div className="md:col-span-1 flex-1 flex flex-col m-8">
+        <div className="mb-8">
+          <CareerLayout />
+        </div>
       </div>
     </div>
   );
